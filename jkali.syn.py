6ec0fec3623a7999284c069ff1b08fa1a80c5bfb
bqcuong <bqcuong2212@gmail.com>
2022-04-12 14:15:59 +0200

    update jkali plugin

--- jkali.syn.py
+++ jkali.syn.py
@@ -23,9 +23,10 @@ class JKali(ToolHandler):
         test_class = repair_args.get('test_class')
         classpath = repair_args.get('classpath')
         jvm_version = repair_args.get('jvm_version')
+        project_name = repair_args.get('project_name')
 
         self.repair_cmd.add_arg('-jvmversion', jvm_version)
-        self.repair_cmd.add_arg('-location', repair_request.working_dir)
+        self.repair_cmd.add_arg('-location', repair_request.working_dir / project_name)
         self.repair_cmd.add_arg('-srcjavafolder', src)
         self.repair_cmd.add_arg('-srctestfolder', test)
         self.repair_cmd.add_arg('-binjavafolder', src_class)
